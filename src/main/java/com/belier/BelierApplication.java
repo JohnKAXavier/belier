f81eb2f897135dc33b97f7c302408c7743c4dbd6
M316337 <jandradx@emeal.nttdata.com>
2023-07-05 20:15:48 -0300

    commit inicial

---
+++ src/main/java/com/belier/BelierApplication.java
@@ -0,0 +1,13 @@
+package com.belier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/belier/BelierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
