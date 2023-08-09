4d41643d35d4c850803244af8dfe4ec8684be8a9
M316337 <jandradx@emeal.nttdata.com>
2023-08-08 23:15:35 -0300

    Added properties on Piece, generate update sql and added new migration

---
+++ src/main/resources/db/migration/V2__alter-table-piece.sql
@@ -0,0 +1,8 @@
+alter table piece modify column factory_price  float(53);
+alter table piece modify column net_value  float(53);
+alter table piece modify column price  float(53);
+alter table piece add column bought_at datetime(6);
+alter table piece modify column factory_price  float(53);
+alter table piece modify column net_value  float(53);
+alter table piece modify column price  float(53);
+alter table piece add column sold_at datetime(6);
